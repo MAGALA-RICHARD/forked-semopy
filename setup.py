8267df7fabaa6deced69730a3a7a0dc2153de279
magala-richard <rmagala640@gmail.com>
2026-02-28 13:36:02 -0600

    fix mvn attribute error and change package dependencies

--- setup.py
+++ setup.py
@@ -1,31 +1,86 @@
 from setuptools import setup, find_packages
 
-
-with open("README.md", "r", encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(	
-      install_requires=['scipy', 'numpy', 'pandas', 'sympy', 'scikit-learn',
-                        'statsmodels', 'numdifftools'],
-      include_package_data=True,
-      package_data={'': ['examples/*.csv', 'examples/*.npy', 'report/*.html', 'report/*.txt',
-					'examples/*.txt', 'report/css/*.css', 'report/js/*.js']},
-      name="semopy",
-      version="2.3.11",
-      author="Georgy Meshcheryakov",
-      author_email="iam@georgy.top",
-      description="Structural Equation Modeling Optimization in Python.",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://semopy.com",
-      packages=find_packages(),
-      python_requires=">=3.7",
-      classifiers=[
-              "Programming Language :: Python :: 3.7",
-	      "Programming Language :: Python :: 3.8",
-	      "Programming Language :: Python :: 3.9",
-	      "Programming Language :: Python :: 3.10",
-              "License :: OSI Approved :: MIT License",
-	      "Development Status :: 5 - Production/Stable",
-	      "Topic :: Scientific/Engineering",
-              "Operating System :: OS Independent"])
+setup(
+    name="semopy",
+    version="2.3.11",
+    author="Georgy Meshcheryakov",
+    author_email="iam@georgy.top",
+    description="Structural Equation Modeling Optimization in Python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://semopy.com",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "": [
+            "examples/*.csv",
+            "examples/*.npy",
+            "examples/*.txt",
+            "report/*.html",
+            "report/*.txt",
+            "report/css/*.css",
+            "report/js/*.js",
+        ]
+    },
+
+    # Proper dependency specification
+    install_requires=[
+        "numpy>=1.23",
+        "scipy>=1.9",
+        "pandas>=1.5",
+        "sympy>=1.10",
+        "scikit-learn>=1.2",
+        "statsmodels>=0.13",
+        "numdifftools>=0.9",
+    ],
+
+    python_requires=">=3.9",
+
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Development Status :: 5 - Production/Stable",
+        "Topic :: Scientific/Engineering",
+        "Operating System :: OS Independent",
+    ],
+)
+
+
+# from setuptools import setup, find_packages
+#
+#
+# with open("README.md", "r", encoding='utf-8') as fh:
+#     long_description = fh.read()
+#
+# setup(
+#       install_requires=['scipy', 'numpy', 'pandas', 'sympy', 'scikit-learn',
+#                         'statsmodels', 'numdifftools'],
+#       include_package_data=True,
+#       package_data={'': ['examples/*.csv', 'examples/*.npy', 'report/*.html', 'report/*.txt',
+# 					'examples/*.txt', 'report/css/*.css', 'report/js/*.js']},
+#       name="semopy",
+#       version="2.3.11",
+#       author="Georgy Meshcheryakov",
+#       author_email="iam@georgy.top",
+#       description="Structural Equation Modeling Optimization in Python.",
+#       long_description=long_description,
+#       long_description_content_type="text/markdown",
+#       url="https://semopy.com",
+#       packages=find_packages(),
+#       python_requires=">=3.7",
+#       classifiers=[
+#               "Programming Language :: Python :: 3.7",
+# 	      "Programming Language :: Python :: 3.8",
+# 	      "Programming Language :: Python :: 3.9",
+# 	      "Programming Language :: Python :: 3.10",
+#               "License :: OSI Approved :: MIT License",
+# 	      "Development Status :: 5 - Production/Stable",
+# 	      "Topic :: Scientific/Engineering",
+#               "Operating System :: OS Independent"])
