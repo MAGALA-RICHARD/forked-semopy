47154ee5497afcfd7db97d588a37b3fbbaa2c3f5
magala-richard <rmagala640@gmail.com>
2026-02-28 12:40:24 -0600

    fix attribute and invalid argument errrors

--- semopy/__init__.py
+++ semopy/__init__.py
@@ -3,21 +3,22 @@
 name = "semopy"
 __version__ = "2.3.11"
 __author__ = "Georgy Meshcheryakov"
-from semopy.semopy.model import Model
-# from .model_generalized_effects import ModelGeneralizedEffects
-# from .model_effects import ModelEffects
-# from .model_means import ModelMeans
-# from .regularization import create_regularization
-# from .stats import calc_stats, gather_statistics
-# from .means import estimate_means
-# from .plot import semplot
-# from .model import Model
-# from .unbias import bias_correction
-# from .optimizer import Optimizer
-# from .report import report
-# from . import model_generation
-# from . import effects
-# from . import multigroup
-# from . import examples
-# from . import utils
-# from . import efa
+
+from .model import Model
+from .model_generalized_effects import ModelGeneralizedEffects
+from .model_effects import ModelEffects
+from .model_means import ModelMeans
+from .regularization import create_regularization
+from .stats import calc_stats, gather_statistics
+from .means import estimate_means
+from .plot import semplot
+from .model import Model
+from .unbias import bias_correction
+from .optimizer import Optimizer
+from .report import report
+from . import model_generation
+from . import effects
+from . import multigroup
+from . import examples
+from . import utils
+from . import efa
